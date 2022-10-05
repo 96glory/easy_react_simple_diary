f4f4baa43ea302fb16f28320fa806913272d292d
96glory <click_syk@naver.com>
2022-10-05 23:05:03 +0900

    React에서 배열 사용하기 1 - 리스트 렌더링 (조회)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id: 1,
+    author: 'glory',
+    content: 'hi 1',
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: 'song',
+    content: 'hi 1222',
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: 'min',
+    content: 'hi 1333',
+    emotion: 7,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: 'toby',
+    content: 'hi 1444',
+    emotion: 8,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 5,
+    author: 'spring',
+    content: 'hi 1555',
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div>
-      <h2>일기장</h2>
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
---
+++ src/DiaryList.js
@@ -0,0 +1,21 @@
+import DiaryItem from './DiaryItem';
+
+const DiaryList = (props) => {
+  const { diaryList } = props;
+
+  return (
+    <div className="DiaryList">
+      <h2>일기 리스트</h2>
+      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+      {diaryList.map((row, idx) => (
+        <DiaryItem key={row.id} {...row} />
+      ))}
+    </div>
+  );
+};
+
+DiaryList.defaultProps = {
+  diaryList: [],
+};
+
+export default DiaryList;
