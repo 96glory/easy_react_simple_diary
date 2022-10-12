f43d8f980a71234e0750a20766a619c3dfb94cdc
youngkwang.song <youngkwang.song@lgcns.com>
2022-10-12 14:52:27 +0900

    최적화 1 - useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect, useMemo, useCallback } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
@@ -10,27 +10,6 @@ function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
-
-  const getData = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json());
-    console.log('res', res);
-
-    const initData = res.slice(0, 20).map((row) => {
-      return {
-        author: row.email,
-        content: row.body,
-        emotion: Math.floor(Math.random() * 5) + 1,
-        created_date: new Date().getTime(),
-        id: dataId.current++,
-      };
-    });
-
-    setData(initData);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -55,10 +34,49 @@ function App() {
     setData(data.map((row) => (row.id === targetId ? { ...row, content: newContent } : row)));
   };
 
+  const getDiaryAnalysis = useMemo(() => {
+    console.log('일기 분석 시작');
+
+    const goodCount = data.filter((row) => row.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100.0;
+
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
+  const getData = useCallback(async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json());
+    console.log('res', res);
+
+    const initData = res.slice(0, 20).map((row) => {
+      const emotion = Math.floor(Math.random() * 5) + 1;
+      const id = dataId.current++;
+      return {
+        author: row.email,
+        content: row.body,
+        emotion: emotion,
+        created_date: new Date().getTime(),
+        id: id,
+      };
+    });
+
+    setData(initData);
+  });
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       {/* <Lifecycle /> */}
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio} %</div>
       <DiaryList onRemove={onRemove} onEdit={onEdit} diaryList={data} />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+/**
+ * 빈 배열 useEffect 시 두번 실행되는 사유
+ * https://velog.io/@hyes-y-tag/React-useEffect%EA%B0%80-%EB%91%90%EB%B2%88-%EC%8B%A4%ED%96%89%EB%90%9C%EB%8B%A4%EA%B3%A0
+ *
+ */
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+root.render(<App />);
